d6877c491c231fa4e2a033e94afccfeabdcc38a1
alexiaCat <alexia_12ceci@hotmail.com>
2023-09-15 20:29:38 -0300

    [update docs]

--- src/QrComponent.jsx
+++ src/QrComponent.jsx
@@ -4,7 +4,7 @@ export const QrComponent = () => {
     return (
         <div className="bg-light-gray w-screen h-screen flex flex-col justify-center items-center ">
             <div className="md:w-[375px] w-[90%] h-[600px] p-4 rounded-xl bg-white shadow-xl">
-                <img className="rounded-xl" src='../public/image-qr-code.png' alt="qr code" width="100%" />
+                <img className="rounded-xl" src='./image-qr-code.png' alt="qr code" width="100%" />
                 <div className="py-5 text-center">
                     <p className="text-dark-blue font-bold font-outfit text-[28px] pb-1">Improve your front-end skills by building proyects</p>
                     <p className="text-grayish-blue font-outfit text-[18px] p-1">Scan the QR code to visit Frontend Mentor and take your coding skills to the netx level</p>
