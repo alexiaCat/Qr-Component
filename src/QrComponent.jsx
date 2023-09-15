d8c1347fa864e0805a428ec20d368b2e568dad1d
alexiaCat <alexia_12ceci@hotmail.com>
2023-09-15 19:15:52 -0300

    [first commit]

---
+++ src/QrComponent.jsx
@@ -0,0 +1,12 @@
+
+
+export const QrComponent = () => {
+    return (
+        <div className="bg-light-gray w-screen h-screen">
+            <div className="w-40% bg-white">
+                <img src='../public/image-qr-code.png' alt="qr code" />
+            </div>
+
+        </div>
+    )
+}
